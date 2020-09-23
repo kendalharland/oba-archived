ad817c6c9d5e16a9bb9f10e75ca126d25eb5353c
Kendal <kendaljharland@gmail.com>
2020-09-23 17:26:51 -0400

    Add REPL scaffolding

--- src/main.c
+++ src/main.c
@@ -1,6 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 
+char* read_line(void) {
+
+}
+
+int eval_line(char* line) {}
+
+
+void repl(void) {
+	char *line;
+	int status;
+
+	do {
+		printf("> ");
+		line = read_line(); // Line may not be a complete expr?
+		status = eval_line(line);
+		free(line);
+	} while (status);
+}
+
 int main(int argc, char **argv) {
+	repl();
 	return EXIT_SUCCESS;
 }
+
+
