d0acb08a32effafeceaffca4ec120fda688df979
Kendal <kendaljharland@gmail.com>
2020-10-12 15:14:54 -0400

    Capture test output

--- tools/test.py
+++ tools/test.py
@@ -1,7 +1,11 @@
-import sys
 import argparse
 import glob
+import os
+import re
 import subprocess
+import sys
+
+TEST_DIR = "test"
 
 
 def get_args():
@@ -13,14 +17,24 @@ def get_args():
     return parser.parse_args()
 
 
-def run_test_files(oba, files):
-    for filename in files:
-        subprocess.run([oba, filename])
+def run_test(oba, test_name, test_file):
+    proc = subprocess.run(
+        [oba, test_file], stderr=subprocess.PIPE, stdout=subprocess.PIPE
+    )
+    print(proc.stdout)
+
+
+def run_test_files(oba, filepaths):
+    for filepath in filepaths:
+        test_name = os.path.relpath(filepath, TEST_DIR)
+        run_test(oba, test_name, filepath)
 
 
 def main():
     args = get_args()
-    test_files = glob.glob("test/" + args.glob, recursive=True)
+
+    test_file_glob = os.path.join(TEST_DIR, args.glob)
+    test_files = glob.glob(test_file_glob, recursive=True)
     run_test_files(args.oba, test_files)
 
 
